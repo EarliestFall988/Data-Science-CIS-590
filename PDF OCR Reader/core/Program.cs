e41f568ccb44611ae84ffb47dbee3cb894506b26
Taylor Howell <howelltaylor195@gmail.com>
2023-02-03 19:00:11 -0600

    wrapping up extraction process

--- PDF OCR Reader/core/Program.cs
+++ PDF OCR Reader/core/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//PDF_PATH
+
+
 Console.WriteLine("Hello, World!");
+Core.PDFExtract extract = new Core.PDFExtract();
+
+await extract.GetFile();
