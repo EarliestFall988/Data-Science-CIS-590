5e87bbb889d11c1999a2922963ab9c0575cbf706
EarliestFall <howelltaylor195@gmail.com>
2023-02-24 12:24:38 -0600

    working on data extraction

--- FormatCSV_PDF_Data/Line.cs
+++ FormatCSV_PDF_Data/Line.cs
@@ -13,6 +13,12 @@ namespace FormatCSV_PDF_Data
         /// <value></value>
         public string RawLine { get; set; } = "";
 
+        /// <summary>
+        /// The line id
+        /// </summary>
+        /// <value></value>
+        public int Id { get; set; } = 0;
+
 
         public string[] Lines => ConvertToLines();
 
@@ -21,14 +27,82 @@ namespace FormatCSV_PDF_Data
 
         }
 
-        public Line(string line)
+        public Line(string line, int id)
         {
             RawLine = line;
+            Id = id;
         }
 
+        /// <summary>
+        /// Convert the line to an array of strings
+        /// </summary>
+        /// <returns></returns>
         public string[] ConvertToLines()
         {
-            return RawLine.Split(',');
+            // var result = RawLine.Split(',');
+
+            // Console.WriteLine(Id);
+            // foreach (var x in result)
+            // {
+            //     Console.WriteLine(x);
+            // }
+
+            // return result;
+
+            return ParseLine(RawLine).ToArray();
+        }
+
+        /// <summary>
+        /// Parse csv line
+        /// </summary>
+        /// <param name="line">the line to parse</param>
+        /// <returns>return the parsed string</returns>
+        private List<string> ParseLine(string line)
+        {
+
+            bool literal = false;
+            List<string> result = new List<string>();
+
+            bool debug = Id == 1;
+            Console.WriteLine(Id);
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '"')
+                {
+                    literal = !literal;
+                }
+
+                if (line[i] == ',' && !literal)
+                {
+                    result.Add(line.Substring(0, i));
+                    line = line.Substring(i + 1);
+
+                    if (debug)
+                        Console.Write(" '" + line + "'");
+
+                    i = 0;
+                }
+            }
+
+            Console.WriteLine();
+            return result;
+        }
+
+        public override string ToString()
+        {
+
+            string result = "";
+
+            string[] data = ConvertToLines();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                result = "\t" + i.ToString() + " " + data[i];
+                
+            }
+
+            return result;
         }
     }
 }
--- FormatCSV_PDF_Data/Program.cs
+++ FormatCSV_PDF_Data/Program.cs
@@ -3,10 +3,11 @@ using System.IO;
 using System;
 using System.Collections.Generic;
 
+using FormatCSV_PDF_Data;
+
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Start");
-
 
 string? path = System.Environment.GetEnvironmentVariable("CSV_PATH", EnvironmentVariableTarget.User);
 
@@ -16,9 +17,63 @@ if (path == null)
 if (path == string.Empty)
     throw new ArgumentException("csv path is empty");
 
-if(!File.Exists(path))
+if (!File.Exists(path))
     throw new FileNotFoundException("csv file not found");
 
-string[] lines = File.ReadAllLines(path);
+string lines = File.ReadAllText(path);
+
+
+bool literal = true;
+List<Line> result = new List<Line>();
+
+for (int i = 0; i < lines.Length; i++)
+{
+    // Console.WriteLine("i = " + i.ToString() + " " + lines[i].ToString());
+    if (lines[i] == '"')
+    {
+        //Console.WriteLine("found a quote");
+        literal = !literal;
+    }
+    else if (lines[i] == '\n' && literal)
+    {
+
+        //Console.WriteLine("found a newline");
+        string newline = lines.Substring(0, i).Trim();
+        //Console.WriteLine(newline);
+
+        result.Add(new Line(newline, i));
+        lines = lines.Substring(i);
+        i = 0;
+    }
+    else if (lines[i] == ',' && literal)
+    {
+        //Console.WriteLine("found a comma");
+        string newline = lines.Substring(0, i).Trim();
+        //Console.WriteLine(newline);
+
+        result.Add(new Line(newline, i));
+        lines = lines.Substring(i);
+        i = 0;
+    }
+    else if (i == lines.Length - 1)
+    {
+        //Console.WriteLine("found the end of the file");
+        string newline = lines.Substring(0, i).Trim();
+        //Console.WriteLine(newline);
+
+        result.Add(new Line(newline, i));
+        lines = lines.Substring(i);
+        i = 0;
+    }
+}
+
+int k = 0;
+foreach (var x in result)
+{
+    Console.WriteLine("\t" + (k++).ToString());
+    Console.WriteLine(x.RawLine);
+}
+
+
 
 Console.WriteLine("end");
